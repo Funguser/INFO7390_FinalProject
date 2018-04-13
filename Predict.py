8d8ecb68a90545685399ac4ee5d913977f750da4
ZiyaoQiao <qiaoziyao@gmail.com>
2018-04-12 20:07:12 -0400

    Add files via upload

---
+++ Predict.py
@@ -0,0 +1,99 @@
+import warnings
+from os.path import split
+import numpy as np
+import pandas as pd
+from glob import glob
+from PIL import Image
+from keras.applications.inception_v3 import preprocess_input
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+from subprocess import check_output
+import keras
+from keras.models import Sequential, load_model
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+train_images = glob(".\\input\\train\\*jpg")
+test_images = glob(".\\input\\test\\*jpg")
+df = pd.read_csv(".\\input\\train.csv")
+
+df["Image"] = df["Image"].map(lambda x: ".\\input\\train\\" + x)
+ImageToLabelDict = dict(zip(df["Image"], df["Id"]))
+SIZE = 224
+
+
+def ImportImage(filename):
+    img = Image.open(filename).convert("LA").resize((SIZE, SIZE))
+    return np.array(img)[:, :, 0]
+
+
+class LabelOneHotEncoder():
+    def __init__(self):
+        self.ohe = OneHotEncoder()
+        self.le = LabelEncoder()
+
+    def fit_transform(self, x):
+        features = self.le.fit_transform(x)
+        return self.ohe.fit_transform(features.reshape(-1, 1))
+
+    def transform(self, x):
+        return self.ohe.transform(self.la.transform(x.reshape(-1, 1)))
+
+    def inverse_tranform(self, x):
+        return self.le.inverse_transform(self.ohe.inverse_tranform(x))
+
+    def inverse_labels(self, x):
+        return self.le.inverse_transform(x)
+
+
+y = list(map(ImageToLabelDict.get, train_images))
+lohe = LabelOneHotEncoder()
+y_cat = lohe.fit_transform(y)
+
+image_gen = ImageDataGenerator(
+    # featurewise_center=True,
+    # featurewise_std_normalization=True,
+    rescale=1. / 255,
+    rotation_range=15,
+    width_shift_range=.15,
+    height_shift_range=.15,
+    horizontal_flip=True)
+
+model = load_model(".\\vgg16-transfer-ver1.model")
+model.load_weights(".\\vgg16-transfer-ver1.model")
+target_size = (224, 224)
+
+def predict(model, img, target_size):
+    """Run model prediction on image
+    Args:
+      model: keras model
+      img: PIL format image
+      target_size: (w,h) tuple
+    Returns:
+      list of predicted labels and their probabilities
+    """
+    if img.size != target_size:
+        img = img.resize(target_size)
+
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    return preds
+
+with open("sample_submission.csv", "w") as f:
+    with warnings.catch_warnings():
+        f.write("Image,Id\n")
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        for images in test_images:
+            img = Image.open(images)
+            img = img.convert("L")
+            img = img.convert("RGB")
+            y = predict(model, img, target_size)
+            predicted_args = np.argsort(y)[0][::-1][:5]
+            predicted_tags = lohe.inverse_labels(predicted_args)
+            images = split(images)[-1]
+            predicted_tags = " ".join(predicted_tags)
+            # if the model is trained without the new_whale class
+            # predicted_tags = "new_whale " + predicted_tags
+            f.write("%s,%s\n" % (images, predicted_tags))
